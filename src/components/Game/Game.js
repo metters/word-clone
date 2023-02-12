96e423e68977abcf514c9df9943f81692154457b
Matthias Streidel <matthias.streidel@senacor.com>
2023-02-12 20:12:46 +0100

    Exercise 5: Set game status

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,20 +4,30 @@ import {sample} from '../../utils';
 import {WORDS} from '../../data';
 import GuessInput from "../GuessInput";
 import GuessResults from "../GuessResults";
+import {NUM_OF_GUESSES_ALLOWED} from "../../constants";
 
 const answer = sample(WORDS);
-console.info({ answer });
+console.info({answer});
 
 function Game() {
     const [guessedResults, setGuessedResults] = React.useState([]);
+    const [gameStatus, setGameStatus] = React.useState('running');
 
-    function handleSubmitGuess(guessInput) {
-        setGuessedResults([...guessedResults, guessInput]);
+    function handleSubmitGuess(input) {
+        const nextGuessedResults = [...guessedResults, input];
+        setGuessedResults(nextGuessedResults);
+
+        if (input.guessInput.toUpperCase() === answer) {
+            setGameStatus('won');
+        } else if (nextGuessedResults.length >= NUM_OF_GUESSES_ALLOWED) {
+            setGameStatus('lost');
+        }
     }
 
     return (
         <>
-            <GuessResults guessedResults={guessedResults} answer={answer}/>
+            {gameStatus}
+            <GuessResults guessedResults={guessedResults} answer={answer} />
             <GuessInput handleSubmitGuess={handleSubmitGuess} />
         </>);
 }
